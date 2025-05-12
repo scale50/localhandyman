2488e982272c704c5910bb8b55abf8da9ae53c32
scale50 <michael@scale50.com>
2025-05-12 09:31:41 +0200

    Update location-finder.js
    added fraser valley codes

--- location-finder.js
+++ location-finder.js
@@ -38,7 +38,7 @@ const zipStateMap = {
 
 const territoryMap = {
     'BC': {
-        'langley': ['V3A', 'V2Y', 'V2Z', 'V1M', 'V4W'],
+        'fraser-valley': ['V3A', 'V2Y', 'V2Z', 'V1M', 'V4W','V4X','V2T','V2S','V3G','V0X','V2R','V2P','V0M','V4Z'],
         'kelowna': ['V1Z', 'V1Y', 'V1X', 'V1W', 'V4V', 'V4T', 'V1V'],
         'vancouver-central': ['V5K', 'V5Y', 'V5W', 'V5V','V59','V6T','V6S','V6N','V6R','V6L','V6K','V6P'],
         'northshore-vancouver': ['V7W', 'V7V', 'V7T', 'V7P', 'V7M', 'V7R', 'V7N', 'V7K','V7G', 'V7H','V7L','V7J','V5Y','V5W','V5V','V5P','V6G','V6E','V7X','V6C','V6Z','V6B','V5T','V6A','V5L'],
@@ -71,7 +71,7 @@ const territoryMap = {
 };
 
 const urlMap = {
-     'langley': 'https://localhandymangroup.com/langley-ojoy',
+    'fraser-valley': 'https://localhandymangroup.com/fraser-valley',
     'kelowna': 'https://localhandymangroup.com/kelowna',
     'regina': 'https://localhandymangroup.com/regina',
     'saskatoon': 'https://localhandymangroup.com/saskatoon',
